edd1421a6201c571ca9a16d7a30a79edccfed598
Diego Licea <diegol@Mac.lan>
2025-12-16 15:35:44 -0800

    Created function to get # of meetings per date

--- js/sketches/viz/viz_title.js
+++ js/sketches/viz/viz_title.js
@@ -2,8 +2,22 @@
 // Draw title-style screens for early active indexes (0 and 1)
 (function () {
     window.VizTitle = {
-        draw: function (p, manager, ai, progress) {
 
+        doneLoading: false,
+
+        preload: function (manager){
+
+            const dateCounts = VizTitle.dateCounts(manager);
+            console.log(dateCounts);
+
+            this.doneLoading = true;
+        },
+
+        draw: function (p, manager, ai, progress) {
+            if (!this.doneLoading){
+                VizTitle.preload(manager);
+            }
+            
             var cx = (manager.offsetX || 0) + (manager.width || 600) / 2;
             var cy = (manager.offsetY || 0) + (manager.height || 520) / 3;
             p.push();
@@ -18,6 +32,26 @@
             p.textSize(48);
             p.text(ai === 0 ? 'INFO 474' : 'Final Project', cx, cy);
             p.pop();
+        },
+
+        dateCounts: function(manager){
+
+            let dateCounts = {};
+
+            for (const meeting of manager.data){
+                let date = meeting["Start time"];
+
+                if (date){
+                    if (dateCounts[date]){
+                        dateCounts[date]++;
+                     } else {
+                        dateCounts[date] = 1;
+                    }
+                }   
+                
+            }
+
+            return dateCounts;
         }
     };
 })();
