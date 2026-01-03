856d460ce2379d33e44b3ee55506607500d0b79c
Diego Licea <diegoalejandrolicea@gmail.com>
2026-01-02 18:31:44 -0800

    removed unnecessary fonts from viz title and viz end

--- js/sketches/viz/viz_title.js
+++ js/sketches/viz/viz_title.js
@@ -7,8 +7,6 @@
         preload: function(p, manager){
 
             this.encodeSans = p.loadFont('fonts/EncodeSansNormal/EncodeSansNormal-900-Black.ttf');
-            this.uniSans = p.loadFont('fonts/UniSans/Uni-Sans-Regular.otf');
-            this.openSans = p.loadFont('fonts/OpenSans/Open-Sans-Regular.ttf');
 
 
             this.doneLoading = true;
